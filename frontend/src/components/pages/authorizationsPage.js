767134701aa3de858aee70d20a5b3ba293c318ad
Gobogabbl <theawesomebug99@gmail.com>
2024-04-01 23:38:31 -0400

    Added a page description into a card on the page

--- frontend/src/components/pages/authorizationsPage.js
+++ frontend/src/components/pages/authorizationsPage.js
@@ -28,25 +28,42 @@ const AuthorizationPage = () => {
                 <h3>
                     Authorizations Page
                 </h3>
-                <h3>
+                <h4>
                     Current User Information
-                </h3>
-                <h3>
+                </h4>
+                <h4>
                     User:
                     <span className='username'> @{username}</span>
-                </h3>
-                <h3>
+                </h4>
+                <h4>
                     UserID: 
                     <span className='userId'> {id}</span>
-                </h3>
-                <h3>
+                </h4>
+                <h4>
                     Email: 
                     <span className='email'> {email}</span>
-                </h3>
+                </h4>
+
+                <Card
+                 body
+                 outline
+                 color="success"
+                 className="mx-1 my-2"
+                 style={{ width: "30rem" }}
+                >
+
+                <Card.Body>
+                <Card.Title>Page Description</Card.Title>
+                <Card.Text>This page is a means in order for the admins to see the different roles
+                    of each user, showing and/or assigning user roles between the existing roles:
+                    User and Admin.
+                </Card.Text>
+                </Card.Body>
+                </Card>
             </div>
-            <button onClick={(e) => handleClick(e)}>
+            {/* <button onClick={(e) => handleClick(e)}>
                 Place Holder
-            </button>
+            </button> */}
         </>
     )
 }
